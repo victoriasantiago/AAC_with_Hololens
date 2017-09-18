961b5ebb9355fd0196b48436e07bcb8ab5143aa4
victoriasantiago <vsantiago18@hotmail.com>
2017-09-17 23:17:10 -0300

    cena 1 e 2 criadas

---
+++ AAC_with_Hololens_project/Assets/Speech.cs
@@ -0,0 +1,30 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speech : MonoBehaviour {
+
+    /// <summary>
+    /// Tap Test for text to speech. This voice will appear to emanate from the object.
+    /// </summary>
+    public string texto;
+    //private TextToSpeech textToSpeech;
+
+        /*private void Awake()
+        {
+            textToSpeech = GetComponent<TextToSpeech>();
+        }*/
+
+        public void OnInputClicked()
+        {
+        // Create message
+        //var msg = string.Format(
+        //"eu", textToSpeech.Voice.ToString());
+        // Speak message
+        //textToSpeech.StartSpeaking(msg);
+            TextToSpeechManager voice = new TextToSpeechManager();
+            voice.SpeakText(texto);
+    }
+    
+}
